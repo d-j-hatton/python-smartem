cd906423e58727b28e8623cf4a718fdcdc89a7cc
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2022-05-22 23:43:51 +0100

    Allow addition of filter based on primary key of last table in list when using convenience function to construct linear queries

--- src/cryotrace/data_model/construct.py
+++ src/cryotrace/data_model/construct.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Type
+from typing import Any, Dict, List, Optional, Type
 
 from sqlalchemy.orm import Query, Session
 
@@ -14,6 +14,10 @@ def _foreign_key(table: Type[Base]) -> str:
     if len(keys) > 1:
         raise ValueError(f"Table {table} has more than one foreign key")
     return keys[0]
+
+
+def _primary_keys(table: Type[Base]) -> List[str]:
+    return [c.name for c in table.__table__.columns if c.primary_key]  # type: ignore
 
 
 def _analyse_table(start: Type[Base]) -> Optional[Type[Base]]:
@@ -41,7 +45,9 @@ def table_chain(start: Type[Base], end: Type[Base]) -> List[Type[Base]]:
     return tables
 
 
-def linear_joins(tables: List[Type[Base]], session: Session) -> Query:
+def linear_joins(
+    session: Session, tables: List[Type[Base]], primary_filter: Any = None
+) -> Query:
     query = session.query(*tables)
     for i, tab in enumerate(tables[:-1]):
         query = query.join(
@@ -49,4 +55,10 @@ def linear_joins(tables: List[Type[Base]], session: Session) -> Query:
             getattr(tab, _foreign_key(tab))
             == getattr(tables[i + 1], _foreign_key(tables[i + 1])),
         )
+    if primary_filter is not None:
+        try:
+            primary_key = _primary_keys(tables[-1])[0]
+        except IndexError:
+            return query
+        query = query.filter(getattr(tables[-1], primary_key) == primary_filter)
     return query
