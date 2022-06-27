d8df46d29149ee95b087cedf35ac8d0ce06e457f
Daniel Hatton <daniel.hatton@diamond.ac.uk>
2022-06-27 17:04:37 +0100

    Fix parse_epu_dir which now needs the project name

--- src/smartem/gui/qt/__init__.py
+++ src/smartem/gui/qt/__init__.py
@@ -242,7 +242,7 @@ class ProjectLoader(ComponentTab):
             raise ValueError(
                 "Project record not found despite having just been inserted"
             )
-        parse_epu_dir(Path(self.epu_dir), self._extractor)
+        parse_epu_dir(Path(self.epu_dir), self._extractor, self._project_name)
         self._main_display._set_epu_directory(Path(self.epu_dir))
         self._main_display._set_data_size(Path(self.project_dir))
         self._main_display.project = self._project_name
